e17c5e644f79fbdaefb8e52805100d2c3e44b568
Allenyzh <allenyangzhenhao@gmail.com>
2024-06-11 21:10:21 -0400

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  plugins: [require("flowbite/plugin")],
   theme: {
     extend: {
       backgroundImage: {
